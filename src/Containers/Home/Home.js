5d565d5b84f504be5a40346f69af7119687c52a3
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-05 06:03:58 -0300

    Creates base folders structure

---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,17 @@
+// Core
+import React, { Component } from 'react';
+
+// Styles
+import styles from './Home.module.css';
+
+class Home extends Component {
+  render = () => {
+    return (
+      <div>
+        <h1 className={styles.title}>Home</h1>
+      </div>
+    );
+  };
+}
+
+export default Home;
