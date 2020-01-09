5eef78490818864a679cb7791d864379de683c89
naueru <adolfonahuel@gmail.com>
2020-01-09 03:17:17 -0300

    Feature/modal container (#14)
    * Updates styles (#11) (#12)
    
    * Adds modal curtain background color
    
    * Creates new base modal component

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,32 @@
+// Core
+import React from 'react';
+
+// Libraries
+import PropTypes from 'prop-types';
+
+// Styles
+import styles from './Modal.module.css';
+
+const Modal = ({child, onClose, children}) => {
+  return (
+    <div
+      className={styles.modalContainer}
+      onClick={onClose}
+    >
+      {child}
+      {children}
+    </div>
+  );
+}
+
+// ToDo: Define by props if uses close button, wrapper and type of transition
+Modal.propTypes = {
+  child: PropTypes.string,
+  onClose: PropTypes.func
+};
+
+Modal.defaultProps = {
+  onClose: () => {}
+}
+
+export default Modal;
