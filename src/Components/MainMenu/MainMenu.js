7ef39e689e9b87ea7767306e9be947bdd3648eb5
naueru <adolfonahuel@gmail.com>
2020-01-08 23:21:15 -0300

    Dev (#10)
    * Removes duplicated end line
    
    * Implements size by constants
    
    * Creates new sizes constants
    
    * Adds status bar translations
    
    * Adds prop types
    
    * Creates new status bar component
    
    * Implements status bar component
    
    * Creates MainBoard base component
    
    * Implements new MainBoard component
    
    * Updates sizes constants
    
    * Refactors Videoplayer and ChatLog
    
    * Implements border
    
    * Implements border
    
    * Updates sizes due to borders
    
    * Updates colors
    
    * Creates new base ChatLog component
    
    * Creates new base VideoPlayer component
    
    * Feature/sub menu (#5)
    
    * Adds z-index for menu
    
    * implements SubMenu components as examples
    
    * Creates new base submenu component
    
    * Feature/sub menu (#7)
    
    * Adds z-index for menu
    
    * implements SubMenu components as examples
    
    * Creates new base submenu component
    
    * Creates new parser for menues
    
    * Implements menu parser
    
    * Removes moched data for component
    
    * Updates styles
    
    * Improves translations
    
    * Feature/sub menu (#9)
    
    * Adds z-index for menu
    
    * implements SubMenu components as examples
    
    * Creates new base submenu component
    
    * Creates new parser for menues
    
    * Implements menu parser
    
    * Removes moched data for component
    
    * Updates styles
    
    * Improves translations
    
    * Dev (#8)
    
    * Removes duplicated end line
    
    * Implements size by constants
    
    * Creates new sizes constants
    
    * Adds status bar translations
    
    * Adds prop types
    
    * Creates new status bar component
    
    * Implements status bar component
    
    * Creates MainBoard base component
    
    * Implements new MainBoard component
    
    * Updates sizes constants
    
    * Refactors Videoplayer and ChatLog
    
    * Implements border
    
    * Implements border
    
    * Updates sizes due to borders
    
    * Updates colors
    
    * Creates new base ChatLog component
    
    * Creates new base VideoPlayer component
    
    * Feature/sub menu (#5)
    
    * Adds z-index for menu
    
    * implements SubMenu components as examples
    
    * Creates new base submenu component
    
    * Feature/sub menu (#7)
    
    * Adds z-index for menu
    
    * implements SubMenu components as examples
    
    * Creates new base submenu component
    
    * Creates new parser for menues
    
    * Implements menu parser
    
    * Removes moched data for component
    
    * Updates styles
    
    * Improves translations
    
    * Implemented basic method to show and hide subMenues
    
    * Adds main menu buttons styles
    
    * Adjusts sub menu styles according to new structure

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -11,14 +11,31 @@ import SubMenu from './SubMenu';
 import styles from './MainMenu.module.css';
 
 class MainMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   renderMenuItems = (list) => {
+    const { state } = this;
     return list.map((item, index) => {
-      const label = item.label;
+      const label     = item.label,
+        stateKey      = `show${label}subMenu`,
+        renderSubmenu = state[stateKey];
       return (
         <li className={styles.mainListItem} key={`${label}_${index}`}>
+          <button
+            className={styles.mainMenuButton}
+            onClick={() => {
+              let newState = state;
+                newState[stateKey] = !newState[stateKey];
+              this.setState({ newState }); // ToDo: Adjust this logic, maybe check for active menu to render
+            }
+          }>
             {label}
-            <SubMenu items={item.subMenues} />
-          </li>
+          </button>
+          {renderSubmenu && <SubMenu items={item.subMenues} />}
+        </li>
       )
     })
   }
