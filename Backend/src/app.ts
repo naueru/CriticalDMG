c77902b365f116e49b38d20b84fe89782ec207c9
Lucas Botteri <lucasbotteri@hotmail.com>
2020-01-11 19:25:27 -0300

    Favicon functionality not required Home page not required

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,5 +1,3 @@
-import path from 'path';
-import favicon from 'serve-favicon';
 import compress from 'compression';
 import helmet from 'helmet';
 import cors from 'cors';
@@ -22,13 +20,13 @@ const app: Application = express(feathers());
 
 // Load app configuration
 app.configure(configuration());
-// Enable security, CORS, compression, favicon and body parsing
+// Enable security, CORS, compression, and body parsing
 app.use(helmet());
 app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+
 // Host the public folder
 app.use('/', express.static(app.get('public')));
 
