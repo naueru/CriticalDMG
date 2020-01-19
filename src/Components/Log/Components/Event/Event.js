ebce9b77f4da16336d597b7f8a382986ef58eced
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-19 10:59:16 -0300

    Creates new base event component

--- src/Components/Log/Components/Event/Event.js
+++ src/Components/Log/Components/Event/Event.js
@@ -26,34 +26,56 @@ class Event extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = {};
+    const { index } = this.props
+    this.state = {
+      audioId: `Event_sound_${index}`
+    };
   }
 
   componentDidMount = () =>{
     const { imgName, imgUrl, sound, autoPlay } = this.props;
-    if (imgName && imgUrl && autoPlay) {this.playSound(sound)};
+
+    if (imgName && imgUrl && autoPlay) {this.showPicture(imgUrl, imgName)};
     if (sound && autoPlay) {this.playSound(sound)};
+
     return;
   };
 
-  playSound = (sound) => {
-    return alert(`Playing event sound: ${sound}`)
+  playSound = (id) => {
+    let audio = document.getElementById(id);
+    if (audio) {
+      audio.play();
+    }
+    return;
+  };
+
+  showPicture = (picture, alt) => {
+    return alert(`Displaying event image: ${picture} alt: ${alt}`);
   };
 
   render = () => {
     const {
       text,
-      // imgUrl,
-      // imgName,
-      // sound
-    } = this.props;
+      imgUrl,
+      imgName,
+      sound,
+    } = this.props,
+    { audioId } = this.state,
+    renderMedia = imgUrl || sound;
     return (
       <div className={styles.eventContainer}>
         <p className={styles.eventText}>{text}</p>
-        <div>
-          <CDMGIcon />
-          <CDMGIcon />
-        </div>
+        {renderMedia && <div className={styles.eventMediaContainer}>
+          {imgUrl && <button onClick={() => this.showPicture(imgUrl, imgName)} className={styles.eventMediaBtn}>
+            <CDMGIcon name={'picture'}/>
+          </button>}
+          {sound  && <button onClick={() => this.playSound(audioId)} className={styles.eventMediaBtn}>
+            <CDMGIcon name={'sound'}/>
+          </button>}
+          {sound && <audio id={audioId} src={sound}>
+            Your browser does not support the <code>audio</code> element.
+          </audio>}
+        </div>}
       </div>
     );
   };
