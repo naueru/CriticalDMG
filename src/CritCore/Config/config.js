9f5d3e620cf89a79a3d63abd4d39fb06305c239e
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-19 22:53:59 -0300

    Adds language and default language config

--- src/CritCore/Config/config.js
+++ src/CritCore/Config/config.js
@@ -1,4 +1,6 @@
 const config = {
+  language: 'es',
+  defaultLanguage: 'en',
   about: {
     docsUrl: 'https://docs.google.com/document/d/1-o3B3WHAnCwsz80m3C76dEQzB4DJ3NoNrvJGZrzN6gc/edit?usp=sharing'
   },
