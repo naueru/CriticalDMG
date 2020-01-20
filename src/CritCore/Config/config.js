7981e636ae2058b4a8925528e2d8fdb76f261efa
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-19 22:30:56 -0300

    Updates about config

--- src/CritCore/Config/config.js
+++ src/CritCore/Config/config.js
@@ -1,5 +1,7 @@
 const config = {
-  docsUrl: 'https://docs.google.com/document/d/1-o3B3WHAnCwsz80m3C76dEQzB4DJ3NoNrvJGZrzN6gc/edit?usp=sharing'
+  about: {
+    docsUrl: 'https://docs.google.com/document/d/1-o3B3WHAnCwsz80m3C76dEQzB4DJ3NoNrvJGZrzN6gc/edit?usp=sharing'
+  }
 };
 
 export default config;
