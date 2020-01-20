dd2664335d9f3bc697fcc5b889204e8533f31243
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-19 22:35:51 -0300

    Adds pictureViewer configuration object

--- src/CritCore/Config/config.js
+++ src/CritCore/Config/config.js
@@ -1,6 +1,9 @@
 const config = {
   about: {
     docsUrl: 'https://docs.google.com/document/d/1-o3B3WHAnCwsz80m3C76dEQzB4DJ3NoNrvJGZrzN6gc/edit?usp=sharing'
+  },
+  pictureViewer: {
+    magnifierSize: '50%'
   }
 };
 
