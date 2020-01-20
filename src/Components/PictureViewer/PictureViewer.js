9376548703a1c25c55491b2268affa4fc90dcdbb
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-19 21:34:18 -0300

    Creates new PictureViewer to replace mapViewer component

---
+++ src/Components/PictureViewer/PictureViewer.js
@@ -0,0 +1,76 @@
+// Core
+import React, { Fragment } from 'react';
+
+// Libraries
+import PropTypes from 'prop-types';
+
+// Components
+import { GlassMagnifier } from "react-image-magnifiers";
+import TabsContainer from '../TabsContainer';
+
+// Styles
+import styles from './PictureViewer.module.css';
+
+const PictureViewer = ({imageList, onClick}) => {
+  const contentList = imageList.map((item) => {
+    const { type, content } = item,
+    { label } = content;
+    let comp;
+
+    switch (type) {
+      case 'image':
+        const { name, imgUrl} = content;
+        comp = {
+          label,
+          content: (
+            <React.Fragment>
+              <h4>{name}</h4>
+              <img src={imgUrl} alt={name} />
+            </React.Fragment>
+          )
+        }
+        break;
+      case 'map':
+        const { smallImgUrl, largeImgUrl } = content;
+        comp = {
+          label,
+          content: (
+            <Fragment>
+              <GlassMagnifier
+                imageSrc={smallImgUrl}
+                imageAlt={label}
+                largeImageSrc={largeImgUrl}
+                magnifierSize="50%"
+                allowOverflow
+              />
+            </Fragment>
+          )
+        }
+        break;
+      default:
+        break;
+    }
+    return comp;
+  });
+
+  return (
+    <div
+      className={styles.pictureViewerContainer}
+      onClick={onClick}
+    >
+      <TabsContainer
+        tabs={contentList}
+      />
+    </div>
+  );
+}
+
+PictureViewer.propTypes = {
+  imageList: PropTypes.array,
+};
+
+PictureViewer.defaultProps = {
+  imageList: []
+}
+
+export default PictureViewer;
