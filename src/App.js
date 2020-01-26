bac90cb0ff9b6af63f2300d99f743149df6074d6
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-26 01:37:57 -0300

    Implements store and provider for app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 // Core
 import React from 'react';
 
+// Store
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { ActionCreators } from './actions';
+
+// Libraries
+import _get from 'lodash/get';
+
 // Components
 import Home from './Containers/Home';
+import Welcome from './Containers/Welcome';
 
 // Styles
 import './App.css';
 
-function App() {
+function App({session}) {
+  const { logged } = session;
   return (
     <div className="App">
-      <Home />
+      {!logged ? <Home /> : <Welcome />}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  // FILTERED PROPS STORE HERE
+  session: state.session,
+  isAuth: _get(state, 'session.isAuth'),
+});
+
+const mapDispatchToProps = (dispatch) => {
+  // Filter used actions
+  const { checkCredentials } = ActionCreators;
+  return bindActionCreators(
+    {
+      // FILTERED ACTIONS HERE
+      checkCredentials,
+    },
+    dispatch,
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
