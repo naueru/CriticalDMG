4bc5c95ddb38b76975f05ce7a86ae6c66d913160
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-26 17:02:10 -0300

    Creates new base login component

---
+++ src/Components/Register/Register.js
@@ -0,0 +1,17 @@
+// React
+import React, { Component } from 'react';
+
+// Styles
+import styles from './Register.module.scss';
+
+class Register extends Component {
+  render = () => {
+    return (
+      <div className={styles.registerContainer}>
+        Registration is a work in progress
+      </div>
+    );
+  };
+};
+
+export default Register;
