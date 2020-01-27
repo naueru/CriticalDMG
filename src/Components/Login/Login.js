fbbd54fdf7bbec4b2cea592ba752d875f381d87a
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-27 20:25:39 -0300

    Implements translations for login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,6 +4,12 @@ import React, { Component } from 'react';
 // Libraries
 import PropTypes from 'prop-types';
 
+// Config
+import config from '../../CritCore/Config/config';
+
+// Translations
+import getTranslations from '../../CritCore/Translations/Translations.js';
+
 // Styles
 import styles from './Login.module.scss';
 
@@ -23,19 +29,29 @@ class Login extends Component {
   }
 
   render = () => {
-    const { userName, pwd, handleChange, onSubmit } = this.props;
+    const { userName, pwd, handleChange, onSubmit } = this.props,
+      { language }      = config,
+      translations      = getTranslations(language), //ToDo: Replace this language for config directly or from store
+      loginTranslations = translations.login,
+      userNameLabel     = loginTranslations.userName,
+      passwordLabel     = loginTranslations.password,
+      submitLabel       = loginTranslations.button;
     return (
       <div className={styles.loginContainer} onClick={e => e.stopPropagation()}>
-        <h3 className={styles.loginHeadline}>User Name</h3>
+        <h3 className={styles.loginHeadline}>{userNameLabel}</h3>
         <input className={styles.logininput} defaultValue={userName} onChange={e => handleChange({userName: e.target.value})} />
-        <h3 className={styles.loginHeadline}>Password</h3>
+        <h3 className={styles.loginHeadline}>{passwordLabel}</h3>
         <input className={styles.logininput} defaultValue={pwd} onChange={e => handleChange({password: e.target.value})} type="password" />
-        <button className={styles.loginBtn} onClick={onSubmit}>Login</button>
+        <button className={styles.loginBtn} onClick={onSubmit}>{submitLabel}</button>
         <div>
-          Test users
-          <button onClick={() => handleChange({userName: 'valdamir', password: '1234'})}>Test user 1</button>
-          <button onClick={() => handleChange({userName: 'meriadoc', password: '1234'})}>Test user 2</button>
-          <button onClick={() => handleChange({userName: 'dilfo', password: '1234'})}>Test user 3</button>
+          <span>
+            Test users:
+          </span>
+          <div>
+            <button onClick={() => handleChange({userName: 'valdamir', password: '1234'})}>User 1</button>
+            <button onClick={() => handleChange({userName: 'meriadoc', password: '1234'})}>User 2</button>
+            <button onClick={() => handleChange({userName: 'dilfo', password: '1234'})}>User 3</button>
+          </div>
         </div>
       </div>
     );
