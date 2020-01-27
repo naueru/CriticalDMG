1a597b154cf81df16cefdff4eafe795df1c259ca
Nahuel Andrada <adolfonahuel@gmail.com>
2020-01-27 20:29:50 -0300

    Fixes Drako alter ego

--- src/mocks/sessionMocks.js
+++ src/mocks/sessionMocks.js
@@ -1,5 +1,5 @@
 export const users = [
   { id: 0, userName: 'valdamir', password: '1234', picture: '', icon: 'warrior', alterEgo: 'Valdamir!' },
   { id: 1, userName: 'meriadoc', password: '1234', picture: '', icon: 'mage', alterEgo: 'Meriadoc' },
-  { id: 2, userName: 'dilfo', password: '1234', picture: '', icon: 'bard', alterEgo: 'Dilfo' }
+  { id: 2, userName: 'dilfo', password: '1234', picture: '', icon: 'bard', alterEgo: 'Drako' }
 ];
