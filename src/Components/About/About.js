59dd18f827b13e066b27c8f7878e0c87895f4946
Nahuel Andrada <adolfonahuel@gmail.com>
2020-02-05 01:54:12 -0300

    Turns About into functional component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,5 @@
 // React
-import React, { Component } from 'react';
+import React from 'react';
 
 // Config
 import config from '../../CritCore/Config/config';
@@ -10,20 +10,18 @@ import getTranslations from '../../CritCore/Translations/Translations.js';
 // Styles
 import styles from './About.module.css';
 
-class About extends Component {
-  render = () => {
-    const { about, language } = config,
-      { docsUrl } = about,
-      translations  = getTranslations(language),
-      { description, docsLinkLabel } = translations.about;
-    return (
-      <div className={styles.aboutContainer}>
-        <h1>CriticalDMG</h1>
-        <p className={styles.aboutDescription}>{description}</p>
-        <a href={docsUrl} target="_blank" rel="noopener noreferrer">{docsLinkLabel}</a>
-      </div>
-    );
-  };
+const About = () => {
+  const { about, language } = config,
+    { docsUrl } = about,
+    translations  = getTranslations(language),
+    { description, docsLinkLabel } = translations.about;
+  return (
+    <div className={styles.aboutContainer}>
+      <h1>CriticalDMG</h1>
+      <p className={styles.aboutDescription}>{description}</p>
+      <a href={docsUrl} target="_blank" rel="noopener noreferrer">{docsLinkLabel}</a>
+    </div>
+  );
 }
 
 export default About;
