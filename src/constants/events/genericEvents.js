cd1e1b9979d73d90f27a6078920ffe84d2f089ea
Nahuel Andrada <adolfonahuel@gmail.com>
2020-02-09 00:14:49 -0300

    Implements translations for prefav events and adds mocked event with sound and image

--- src/constants/events/genericEvents.js
+++ src/constants/events/genericEvents.js
@@ -1,5 +1,41 @@
-const prefabEvents = {
-  rain: 'Suddently starts raining'
+// Translations
+import getTranslations from '../../CritCore/Translations/Translations.js';
+
+import snd from '../../assets/sounds/mocks/door.ogg';
+
+const getPrefabEvents = (lang) => {
+  const translatedEvents = getTranslations(lang).events;
+
+  return {
+    softRain: {
+      type: 'event',
+      content:{
+        text: translatedEvents.softRain,
+        autoPlay: false
+      }
+    },
+    heavyRain: {
+      type: 'event',
+      content:{
+        text: 'Suddenly a bunch of thunders and a lot of water start falling from the sky.',
+        autoPlay: false
+      }
+    },
+    beefy: {
+      type: 'event',
+      content:{
+        text: translatedEvents.heavyRain,
+        image: {
+          label: 'Do it!',
+          title: 'Just do it!',
+          url: 'http://cdn01.cdn.justjared.com/wp-content/uploads/headlines/2015/01/shia-labeouf-goes-shirtless-dances-in-a-cage-for-sias-elastic-hart.jpg',
+          name: 'beefy'
+        },
+        sound: snd,
+        autoPlay: true
+      }
+    }
+  };
 };
 
-export default prefabEvents;
+export default getPrefabEvents;
