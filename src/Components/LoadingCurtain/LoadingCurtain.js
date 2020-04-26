a4fcee38051f6326549174cae27ed10adcc7371c
Nahuel Andrada <adolfonahuel@gmail.com>
2020-04-26 17:43:04 -0300

    Creates new base loading curtain component#

---
+++ src/Components/LoadingCurtain/LoadingCurtain.js
@@ -0,0 +1,21 @@
+// React
+import React from 'react';
+
+// Config
+// import config from '../../CritCore/Config/config';
+
+// Translations
+// import getTranslations from '../../CritCore/Translations/Translations.js';
+
+// Styles
+import styles from './LoadingCurtain.module.css';
+
+const LoadingCurtain = () => {
+  return (
+    <div className={styles.LoadingContainer}>
+      Loading...
+    </div>
+  )
+}
+
+export default LoadingCurtain;
