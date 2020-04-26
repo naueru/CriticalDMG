942bdfd5e66366c5c8c74451efa166108ba16e0e
Nahuel Andrada <adolfonahuel@gmail.com>
2020-04-26 00:38:55 -0300

    Implements router and adds routes and components for about-us and 404

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { ActionCreators } from './actions';
 
+// Router
+import { Switch, Route } from 'react-router-dom';
+
 // Libraries
 import _get from 'lodash/get';
 
 // Components
 import Home from './Containers/Home';
 import Welcome from './Containers/Welcome';
+import AboutUs from './Components/AboutUs';
+import NotFound from './Components/NotFound';
 
 // Styles
 import './App.css';
@@ -26,11 +31,16 @@ function App({ isAuth, user, fetchLoggedUser }) {
     }
   });
 
-  const loggedInContent = noUserInRedux ? 'Loading...' : <Home />;
+  const loggedInContent = noUserInRedux ? 'Loading...' : Home;
+  const main = isAuth ? loggedInContent : Welcome;
 
   return (
     <div className="App">
-      {isAuth ? loggedInContent : <Welcome />}
+      <Switch>
+        <Route exact path="/" component={main} />
+        <Route exact path="/about-us" component={AboutUs} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
