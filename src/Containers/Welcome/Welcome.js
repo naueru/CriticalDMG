a45bddb80065f81849e35b18398477456dde4d90
naueru <adolfonahuel@gmail.com>
2020-04-26 20:26:52 -0300

    Improves components and fixes some warnings (#59)
    * Adds router dependencies
    
    * Implements router and adds routes and components for about-us and 404
    
    * Removes blank lines
    
    * Implements connected router and history
    
    * Implements routerMiddleware and history
    
    * Creates new base component for about-us route
    
    * Creates new base component for 404 route
    
    * Removes unused About component
    
    * implements loadingCurtain component
    
    * Updates component to functional
    
    * Updates component to function
    
    * Cleanes up code
    
    * Removes console.log
    
    * Creates new base loading curtain component#
    
    * Implements a hack to avoid some warnings
    
    * Turns Home into functional component and implements hooks
    
    * Turns Welcome into functional component

--- src/Containers/Welcome/Welcome.js
+++ src/Containers/Welcome/Welcome.js
@@ -1,5 +1,5 @@
 // React
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 // Store
 import { bindActionCreators } from 'redux';
@@ -23,98 +23,92 @@ import Register from '../../Components/Register';
 // Styles
 import styles from './Welcome.module.css';
 
-class Welcome extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      showModal: false,
-      showLogin: false,
-      showRegister: false
-    };
-  }
+const Welcome = ({ checkCredentials, registerUser, session = {} }) => {
+  const [ state, setState ] = useState({
+    showModal: false,
+    showLogin: false,
+    showRegister: false
+  });
 
-  handleState = (state) => {
-    return this.setState(state);
+  const handleState = (state) => {
+    return setState(state);
   };
 
-  checkLoginCredentials = ({ userName, password }) => {
-    const { checkCredentials } = this.props;
+  const checkLoginCredentials = ({ userName, password }) => {
     return checkCredentials({ userName, password });
   };
 
-  registerUser = ({
+  const handleRegisterUser = ({
     email,
     userName,
     alterEgo,
     password,
     icon }) => {
-      const { registerUser } = this.props,
-      data = { email,
+      const data = {
+        email,
         userName,
         alterEgo,
         password,
-        icon };
+        icon
+      };
     return registerUser(data);
   };
 
-  render = () => {
-    const {
-        showModal,
-        showLogin,
-        showRegister
-      }                   = this.state,
-      { session = {} }    = this.props,
-      error               = session.error || {},
-      errorCode           = error.code,
-      { language }        = config,
-      translations        = getTranslations(language) || {}, //ToDo: Replace this language for config directly or from store
-      welcomeTranslations = translations.welcome || {},
-      loginLabel          = welcomeTranslations.login,
-      registerLabel       = welcomeTranslations.register,
-      description         = welcomeTranslations.description,
-      errorLabel          = errorCode && welcomeTranslations[errorCode];
-    return (
-      <div className={styles.welcomeContainer}>
-        {showModal && <Modal onClose={() => this.handleState({ showModal: false, showLogin: false, showRegister: false })}>
-          {showLogin && <Login
-            onSubmit={this.checkLoginCredentials}
-            errorLabel={errorLabel}
-          />}
-          {showRegister && <Register
-            onSubmit={this.registerUser}
-          />}
-        </Modal>}
-        <nav className={styles.loginRegisterContainer}>
-          <ul className={styles.loginRegisterList}>
-            <li className={styles.loginRegisterItem}>
-              <button
-                className={styles.loginRegisterButton}
-                onClick={() => this.handleState({ showLogin: true, showModal: true })}
-              >
-                {loginLabel}
-              </button>
-            </li>
-            <span  className={styles.loginRegisterSeparator}>/</span>
-            <li className={styles.loginRegisterItem}>
-              <button
-                className={styles.loginRegisterButton}
-                onClick={() => this.handleState({ showRegister: true, showModal: true })}
-              >
-                {registerLabel}
-              </button>
-            </li>
-          </ul>
-        </nav>
-        <section className={styles.welcomeWrapper}>
-          <div className={styles.welcomeFrame}>
-            <div className={styles.logo} />
-            <h1 className={styles.headline}>CriticalDMG</h1>
-            <p className={styles.description}>{description}</p>
-          </div>
-        </section>
-      </div>
-    );
-  };
+  const {
+      showModal,
+      showLogin,
+      showRegister
+    }                   = state,
+    error               = session.error || {},
+    errorCode           = error.code,
+    { language }        = config,
+    translations        = getTranslations(language) || {}, //ToDo: Replace this language for config directly or from store
+    welcomeTranslations = translations.welcome || {},
+    loginLabel          = welcomeTranslations.login,
+    registerLabel       = welcomeTranslations.register,
+    description         = welcomeTranslations.description,
+    errorLabel          = errorCode && welcomeTranslations[errorCode];
+  return (
+    <div className={styles.welcomeContainer}>
+      {showModal && <Modal onClose={() => handleState({ showModal: false, showLogin: false, showRegister: false })}>
+        {showLogin && <Login
+          onSubmit={checkLoginCredentials}
+          errorLabel={errorLabel}
+        />}
+        {showRegister && <Register
+          onSubmit={handleRegisterUser}
+        />}
+      </Modal>}
+      <nav className={styles.loginRegisterContainer}>
+        <ul className={styles.loginRegisterList}>
+          <li className={styles.loginRegisterItem}>
+            <button
+              className={styles.loginRegisterButton}
+              onClick={() => handleState({ showLogin: true, showModal: true })}
+            >
+              {loginLabel}
+            </button>
+          </li>
+          <span  className={styles.loginRegisterSeparator}>/</span>
+          <li className={styles.loginRegisterItem}>
+            <button
+              className={styles.loginRegisterButton}
+              onClick={() => handleState({ showRegister: true, showModal: true })}
+            >
+              {registerLabel}
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <section className={styles.welcomeWrapper}>
+        <div className={styles.welcomeFrame}>
+          <div className={styles.logo} />
+          <h1 className={styles.headline}>CriticalDMG</h1>
+          <p className={styles.description}>{description}</p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
