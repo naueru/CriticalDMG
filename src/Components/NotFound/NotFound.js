db89c0827d3a41d1fe31552ec27f30b3df57f433
Nahuel Andrada <adolfonahuel@gmail.com>
2020-04-26 17:41:15 -0300

    Updates component to function

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,6 +1,6 @@
 
 // React
-import React, { Component } from 'react';
+import React from 'react';
 
 // Config
 // import config from '../../CritCore/Config/config';
@@ -11,14 +11,12 @@ import React, { Component } from 'react';
 // Styles
 import styles from './NotFound.module.css';
 
-class NotFound extends Component {
-  render = () => {
-    return (
-      <div className={styles.notFoundContainer}>
-        The page you are looking for is not here
-      </div>
-    );
-  };
+const NotFound = () => {
+  return (
+    <div className={styles.notFoundContainer}>
+      The page you are looking for is not here
+    </div>
+  );
 }
 
 export default NotFound;
