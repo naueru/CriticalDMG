69db9776d53405921acf61f00582aff1c89e27db
Jimmy <bruno_pier@hotmail.com>
2020-05-12 12:44:39 -0300

    Adds pwd settings

--- src/CritCore/Config/config.js
+++ src/CritCore/Config/config.js
@@ -1,8 +1,22 @@
 const config = {
-  language: 'en',
-  defaultLanguage: 'en',
   about: {
     docsUrl: 'https://docs.google.com/document/d/1-o3B3WHAnCwsz80m3C76dEQzB4DJ3NoNrvJGZrzN6gc/edit?usp=sharing'
+  },
+  defaultLanguage: 'en',
+  language: 'en',
+  passwordSettings: {
+    email: {
+      minLengthEmail: '7',
+      maxLengthEmail: '60',
+    },
+    pwd: {
+      minLengthPwd: '4',
+      maxLengthPwd: '30',
+    },
+    userName: {
+      minLengthUserName: '6',
+      maxLengthUserName: '20',
+    },
   },
   pictureViewer: {
     magnifierSize: '50%'
@@ -10,4 +24,5 @@ const config = {
   SERVER_HOST: process.env.REACT_APP_SERVER_HOST || "localhost",
   SERVER_PORT: process.env.REACT_APP_SERVER_PORT || "3030",
 };
+
 export default config;
