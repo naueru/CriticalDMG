69a1d58d330341919d55d1a2a094d95ed188fd33
Nahuel Andrada <adolfonahuel@gmail.com>
2020-06-04 03:17:10 -0300

    Implements components for about-us page

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -8,13 +8,25 @@ import React from 'react';
 // Translations
 // import getTranslations from '../../CritCore/Translations/Translations.js';
 
+// Components
+import AboutUsIntroduction from './components/aboutUsIntroduction';
+import AboutUsSlider from './components/aboutUsSlider';
+import AboutUsContributors from './components/aboutUsContributors';
+import AboutUsFooter from './components/aboutUsFooter';
+
 // Styles
 import styles from './AboutUs.module.css';
+
+// Contributors
+import contributors from './contributorsConstants';
 
 const AboutUs = () => {
   return (
     <div className={styles.aboutUsContainer}>
-      It's all about us
+      <AboutUsIntroduction />
+      <AboutUsSlider />
+      <AboutUsContributors contributors={contributors} />
+      <AboutUsFooter />
     </div>
   );
 }
