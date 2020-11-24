7d4af5362bca22b8a45c10735ed0860d9312bcbc
naueru <adolfonahuel@gmail.com>
2020-11-24 18:21:14 -0300

    Dev (#90)
    * Bug/used credentials (#76)
    
    * Adds event to handle error in Register
    
    * Adds .catch code to handle registry error - Actions
    
    * Adds translations for error 400
    
    * Remove unnecessary comment
    
    * Adds specific message translations for code 600 & 601 (#78)
    
    * Adds specific message translations for code 600 & 601
    
    * Adds default error message
    
    * Feature/about us (#80)
    
    * Creates logo gray 10%
    
    * Creates mocked img for carousel
    
    * Creates contributors profile pictures
    
    * Adds new line at the end of file
    
    * Adds carousel dependency
    
    * Adds acrousel styles
    
    * Implements branding component
    
    * Removes logo styles
    
    * Adds styles for container
    
    * Implements components for about-us page
    
    * Creates new logo component
    
    * Creates new branding component
    
    * Creates new characterSheet component conversationalDots
    
    * Creates new characterSheet component rowList
    
    * implements translations
    
    * Creates new introduction component for about us page
    
    * Fixes indentations
    
    * Creates new slider component for about us page
    
    * Creates new contributors component for about us page
    
    * Creates new card component for contributors in about-us section
    
    * Creates new characterSheet component for contributors in about-us page
    
    * Creates contributor constants for about us page
    
    * Creates new footer component for about us page
    
    * Adds translations for about us section
    
    * Fixes carousel styles source
    
    * Fixes description
    
    * Fixes component losing references
    
    * Fixes visual bug for main menu (#85)
    
    * Tentatively fixes infinite loading when the session is not manually closed and the apop is reopoened a while (#87)
    
    * Feature/roll tooltip (#89)
    
    * Converts Roll component from class into functional
    
    * Turns Roll class component into funcional component. Implements new tooltip component
    
    * Creates new Tooltip component
    
    * Adds a new style color variable
    
    * Adds styles for tooltip
    
    * Fixes maximum width and height and implement tooltip display based on content
    
    * Adds Tooltip in Roll
    
    * Adds variables for peak color
    
    * Adds propTypes and defaultProps
    
    * Fixes some bugs - Adds new styles
    
    Co-authored-by: jimmy404 <54155513+jimmy404@users.noreply.github.com>

--- src/Components/Log/Components/Roll/Roll.js
+++ src/Components/Log/Components/Roll/Roll.js
@@ -1,61 +1,61 @@
 // Core
-import React, { Component } from 'react';
+import React from 'react';
 
 // Libraries
 import PropTypes from 'prop-types';
 
 // Components
 import CDMGIcon from '../../../CDMGIcon';
+import Tooltip from '../../../Tooltip';
 
 // Styles
 import styles from './Roll.module.css';
 
-class Roll extends Component {
-  static propTypes = {
-    total: PropTypes.number,
-    results: PropTypes.array,
-    dices: PropTypes.number,
-    faces: PropTypes.number,
-    modifier: PropTypes.number,
-    character: PropTypes.string
-  };
+const Roll = ({
+    total,
+    results,
+    dices,
+    faces,
+    modifier,
+    character
+  }) => {
 
-  static defaultProps = {
-    total: 0,
-    results: [],
-    dices: 0,
-    faces: 0,
-    modifier: 0,
-    character: ''
-  };
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render = () => {
-    const {
-      total,
-      results,
-      dices,
-      faces,
-      modifier,
-      character
-    }       = this.props,
-    mod     = ((modifier >= 0) && `+${modifier}`) || modifier,
+  const mod = ((modifier >= 0) && `+${modifier}`) || modifier,
     rolls   = `[${results.join(', ')}]`,
     prefix  = `${character} rolled `,
     sufix   = ` (${dices}d${faces}${mod})`;
-    return (
-      <div className={styles.rollContainer} >
-        <span className={styles.pictureContainer}>
-          <CDMGIcon name={'dice'} />
-        </span>
-        <p className={styles.rollText}>
-          {prefix}<span className={styles.rollResults} title={rolls}>{total}</span>{sufix}
-        </p>
-      </div>
-    );
-  };
+  return (
+    <div className={styles.rollContainer} >
+      <span className={styles.pictureContainer}>
+        <CDMGIcon name={'dice'} />
+      </span>
+      <p className={styles.rollText}>
+        {prefix}
+        <Tooltip message={rolls} orientation="top" maxWidth="100px" maxHeight="50px">
+          <span className={styles.rollResults}>{total}</span>
+        </Tooltip>
+        {sufix}
+      </p>
+    </div>
+  );
 }
+
+Roll.propTypes = {
+  total: PropTypes.number,
+  results: PropTypes.array,
+  dices: PropTypes.number,
+  faces: PropTypes.number,
+  modifier: PropTypes.number,
+  character: PropTypes.string
+};
+
+Roll.defaultProps = {
+  total: 0,
+  results: [],
+  dices: 0,
+  faces: 0,
+  modifier: 0,
+  character: ''
+};
 
 export default Roll;
