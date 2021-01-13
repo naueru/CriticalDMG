01efb4f105e1e49ce91c9c5b28f098ad9e6583e7
Jimmy404 <bruno_pier@hotmail.com>
2021-01-13 19:30:57 -0300

    Adds mock-up dummy data

---
+++ src/services/dataMock.js
@@ -0,0 +1,18 @@
+const dataMock = {
+  accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyJ9.eyJpYXQiOjE2MTA1NjgzMzgsImV4cCI6MTYxMDY1NDczOCwiYXVkIjoiaHR0cHM6Ly95b3VyZG9tYWluLmNvbSIsImlzcyI6ImZlYXRoZXJzIiwic3ViIjoiNCIsImp0aSI6IjM4OGEyNjBiLWMxYmItNGNjMS1hZWJkLTc2ZTcyZTg0NDEzNiJ9.8Z2tjhJpuT73jTaJHO0TXbqW5gm0rxnTnTPuiSJdyOM',
+  authentication: {
+    strategy: 'local'
+  },
+  user: {
+    id: 4,
+    email: 'g@h.com',
+    userName: 'yvaine',
+    alterEgo: 'Yvaine',
+    picture: null,
+    icon: 'archer',
+    createdAt: '2020-11-29T21:19:59.545Z',
+    updatedAt: '2020-11-29T21:19:59.545Z'
+  }
+}
+
+export default dataMock;
