874753153dc17e710e1e7f965adfe138a5dc21a3
Jimmy200 <bruno_pier@hotmail.com>
2021-02-19 14:09:47 -0300

    Turns CommandInput class component into funcional component

--- src/Components/CommandInput/CommandInput.js
+++ src/Components/CommandInput/CommandInput.js
@@ -1,5 +1,5 @@
 // Core
-import React, { Component } from 'react';
+import React from 'react';
 
 // Libraries
 import PropTypes from 'prop-types';
@@ -7,68 +7,70 @@ import PropTypes from 'prop-types';
 // Styles
 import styles from './CommandInput.module.css';
 
-class CommandInput extends Component {
-  static propTypes = {
-    handleSubmit: PropTypes.func,
-    handleHistory: PropTypes.func,
-    placeholder: PropTypes.string,
-    defaultInput: PropTypes.string,
-    handleChange: PropTypes.func
-  };
+const CommandInput = ({
+  handleSubmit,
+  handleHistory,
+  handleChange,
+  placeholder,
+  defaultInput,
+}) => {
 
-  static defaultProps = {
-    handleSubmit: () => {},
-    handleHistory: () => {},
-    placeholder: '',
-    defaultInput: '',
-    handleChange: () => {}
-  };
-
-  handleChange = (e = {}) => {
+  const handleChanges = (e = {}) => {
     e.preventDefault();
-    const { handleChange } = this.props,
-      { target } = e,
-      { value } = target;
+    const { target } = e,
+          { value }  = target;
     handleChange(value);
   };
 
-  handleKeyDown = (e = {}) => {
-    const { handleSubmit, handleHistory } = this.props,
-      { value } = e.target;
+  const handleKeyDown = (e = {}) => {
+    const { value } = e.target;
     switch(e.key){
       case 'Enter':
         handleSubmit(value);
         e.target.value = '';
         break;
       case 'ArrowUp':
-        handleHistory('decrease', this.setInputValue);
+        handleHistory('decrease', setInputValue);
         break;
       case 'ArrowDown':
-        handleHistory('increase', this.setInputValue);
+        handleHistory('increase', setInputValue);
         break;
       default:
-        break
+        break;
     }
   };
 
-  setInputValue = (val) => {
+  const setInputValue = (val) => {
     let inp = document.getElementById('commandInput');
     inp.value = val;
   };
 
-  render = () => {
-    const { placeholder, defaultInput } = this.props;
-    return (
-      <input
-        id="commandInput"
-        defaultValue={defaultInput}
-        placeholder={placeholder}
-        className={styles.inputText}
-        onKeyDown={this.handleKeyDown}
-        onChange={this.handleChange}
-      />
-    );
-  };
-}
+  return (
+    <input
+      id="commandInput"
+      defaultValue={defaultInput}
+      placeholder={placeholder}
+      className={styles.inputText}
+      onKeyDown={handleKeyDown}
+      onChange={handleChanges}
+    />
+  );
+};
+
+CommandInput.propTypes = {
+  handleSubmit: PropTypes.func,
+  handleHistory: PropTypes.func,
+  placeholder: PropTypes.string,
+  defaultInput: PropTypes.string,
+  handleChange: PropTypes.func,
+};
+
+CommandInput.defaultProps = {
+  handleSubmit: () => {},
+  handleHistory: () => {},
+  placeholder: '',
+  defaultInput: '',
+  handleChange: () => {},
+};
 
 export default CommandInput;
