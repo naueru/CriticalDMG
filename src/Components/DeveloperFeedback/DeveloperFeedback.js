5bc4d747b541bbec8438ee0958a4ffa89bf7c455
Gero <aikanaropal10@gmail.com>
2021-09-30 15:43:35 -0300

    Upgrade developerFeedBack.js

--- src/Components/DeveloperFeedback/DeveloperFeedback.js
+++ src/Components/DeveloperFeedback/DeveloperFeedback.js
@@ -4,9 +4,10 @@ import React from 'react';
 // Styles
 import styles from './DeveloperFeedback.module.css';
 
-const DeveloperFeedback = ({children}) => {
+const DeveloperFeedback = ({children, orientation='left'}) => {
+  const classNames = `${styles.devFeedback} ${styles[orientation]}`;
   return (
-    <pre className={styles.devFeedback}>{children}</pre>
+    <pre className={classNames}>{children}</pre>
   );
 };
 
